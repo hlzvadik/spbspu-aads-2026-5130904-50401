540dc9aa891cd689e0246e6ac82a870a29f59c93
vadik <vadik@debian>
2026-03-03 21:07:16 +0300

    Smth that work:)

--- goltsov.vadim/S1/main.cpp
+++ goltsov.vadim/S1/main.cpp
@@ -5,6 +5,18 @@ int main()
 {
   goltsov::List< int > a;
   a.push_back(1);
+  a.push_back(2);
+  a.push_back(3);
   goltsov::LIter< int > ai = a.begin();
-  std::cout << *ai;
+  ai = ai.next();
+  a.insert(ai, 10);
+  ai = a.begin();
+  ai = a.begin();
+  ai.insert(20);
+  while(ai.hasNext())
+  {
+    std::cout << *ai << '\n';
+    ai = ai.next();
+  }
+  std::cout << *ai << '\n';
 }
