f3dd24f26051fbe459c1413504b4ee8ab84011af
vadik <vadik@debian>
2026-03-07 02:49:08 +0300

    Some changes

--- goltsov.vadim/S1/functions.hpp
+++ goltsov.vadim/S1/functions.hpp
@@ -4,8 +4,10 @@
 #include <string>
 namespace goltsov
 {
-  goltsov::List< std::pair< std::string, goltsov::List< unsigned long long > > > getData(std::istream& in, size_t& size);
+  goltsov::List< std::pair< std::string,
+    goltsov::List< unsigned long long > > > getData(std::istream& in, size_t& size);
   void push_back(size_t** sums, size_t& n, size_t a);
-  std::ostream& printResult(std::ostream& a, goltsov::List< std::pair< std::string, goltsov::List< unsigned long long > > >& data, size_t size);
+  std::ostream& printResult(std::ostream& a, goltsov::List< std::pair< std::string,
+    goltsov::List< unsigned long long > > >& data, size_t size);
 }
 #endif
