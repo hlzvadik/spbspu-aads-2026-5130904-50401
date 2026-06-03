d6ff032ec5786c996a856dd1e47466eaa65c8597
hlzvadik <golcovv716@gmail.com>
2026-06-04 00:54:37 +0300

    Fix mistakes

--- goltsov.vadim/S1/data_processing.cpp
+++ goltsov.vadim/S1/data_processing.cpp
@@ -58,33 +58,30 @@ std::ostream& goltsov::printResult(std::ostream& out, List< std::pair< std::stri
     return out;
   }
   List< LIter< size_t > > list_its;
-  printResultNamesAndMakeListOfIterators(out, data, size, list_its);
-  printResultNumbersAndSums(out, data, size, list_its);
+  printResultNamesAndMakeListOfIterators(out, data, list_its);
+  printResultNumbersAndSums(out, list_its);
   return out;
 }
 
 std::ostream& goltsov::printResultNamesAndMakeListOfIterators(std::ostream& out, goltsov::List< std::pair< std::string,
-  goltsov::List< size_t > > >& data, size_t size, List< LIter< size_t > >& list_its)
+  goltsov::List< size_t > > >& data, List< LIter< size_t > >& list_its)
 {
   LIter< std::pair< std::string, List< size_t > > > it = data.begin();
   LIter< LIter< size_t > > it_list_its = list_its.begin();
   out << (*it).first;
   it_list_its = list_its.insert(it_list_its, (*it).second.begin());
   ++it;
-  for (size_t i = 1; i < size; ++i)
+  for (; it != data.end(); ++it)
   {
     out << ' ' << (*it).first;
     it_list_its = list_its.insert(it_list_its, (*it).second.begin());
-    ++it;
   }
   out << '\n';
   return out;
 }
 
-std::ostream& goltsov::printResultNumbersAndSums(std::ostream& out, goltsov::List< std::pair< std::string,
-  goltsov::List< size_t > > >& data, size_t size, List< LIter< size_t > >& list_its)
+std::ostream& goltsov::printResultNumbersAndSums(std::ostream& out, List< LIter< size_t > >& list_its)
 {
-  LIter< std::pair< std::string, List< size_t > > > it = data.begin();
   bool all = 0;
   bool without_overflows = 1;
   List< size_t > sums;
--- goltsov.vadim/S1/data_processing.hpp
+++ goltsov.vadim/S1/data_processing.hpp
@@ -7,11 +7,10 @@ namespace goltsov
   size_t sumWithCheck(size_t a, size_t b);
   void getData(std::istream& in, List< std::pair< std::string, List< size_t > > >& result, size_t& size);
   void push_back(size_t** sums, size_t& n, size_t a);
-  std::ostream& printResult(std::ostream& a, goltsov::List< std::pair< std::string,
+  std::ostream& printResult(std::ostream& out, goltsov::List< std::pair< std::string,
     goltsov::List< size_t > > >& data, size_t size);
-  std::ostream& printResultNamesAndMakeListOfIterators(std::ostream& a, goltsov::List< std::pair< std::string,
-    goltsov::List< size_t > > >& data, size_t size, List< LIter< size_t > >& list_its);
-  std::ostream& printResultNumbersAndSums(std::ostream& a, goltsov::List< std::pair< std::string,
-    goltsov::List< size_t > > >& data, size_t size, List< LIter< size_t > >& list_its);
+  std::ostream& printResultNamesAndMakeListOfIterators(std::ostream& out, goltsov::List< std::pair< std::string,
+    goltsov::List< size_t > > >& data, List< LIter< size_t > >& list_its);
+  std::ostream& printResultNumbersAndSums(std::ostream& out, List< LIter< size_t > >& list_its);
 }
 #endif
